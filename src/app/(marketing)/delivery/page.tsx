0237ab17511c0df31ad9e21184e377668d8effe5
Nikita Khrulev <dubblerner.2@gmail.com>
2026-04-14 11:50:24 +0300

    fix: delivery layout fixes

--- src/app/(marketing)/delivery/page.tsx
+++ src/app/(marketing)/delivery/page.tsx
@@ -34,23 +34,11 @@ export default function DeliveryPage() {
             </div>
 
             <div className={styles.cardBanner}>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/images/delivery-ozon-banner.jpg" alt="" className={styles.cardBannerBg} />
-              <div className={styles.cardBannerOverlay} />
-
-              <div className={styles.cardBannerContent}>
-                <div>
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src="/images/logo-ozon-white.png" alt="OZON" className={styles.ozonLogo} />
-                  <p className={styles.bannerTitle}>
-                    Доставка
-                    <br />
-                    по всей России
-                  </p>
-                  <p className={styles.bannerSubtitle}>Более 75 000 пунктов выдачи Ozon</p>
-                </div>
-                <p className={styles.bannerFootnote}>Средний срок доставки — от 1 до 3 дней</p>
-              </div>
+              {}
+              <picture>
+                <source media="(max-width: 768px)" srcSet="/images/ozon-delivery-mob.png" />
+                <img src="/images/ozon-delivery.png" alt="" className={styles.cardBannerBg} />
+              </picture>
             </div>
           </div>
 
@@ -64,21 +52,11 @@ export default function DeliveryPage() {
             </div>
 
             <div className={styles.cdekBanner}>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/images/delivery-cdek-banner.jpg" alt="" className={styles.cdekBannerBg} />
-              <div className={styles.cdekContent}>
-                <p className={styles.cdekLabel}>
-                  СДЭК доставка. С помощью ТК СДЭК
-                  <br />
-                  до ПВЗ или курьером до двери
-                </p>
-                <div>
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src="/images/logo-cdek.png" alt="СДЭК" className={styles.cdekLogo} />
-                  <p className={styles.cdekBigText}>Доставка по всей России</p>
-                </div>
-                <p className={styles.cdekNote}>*Срок зависит от города доставки</p>
-              </div>
+              {}
+              <picture>
+                <source media="(max-width: 768px)" srcSet="/images/cdek-delivery-mob.png" />
+                <img src="/images/cdek-delivery.png" alt="" className={styles.cdekBannerBg} />
+              </picture>
             </div>
           </div>
         </div>
@@ -93,28 +71,20 @@ export default function DeliveryPage() {
         <div className={styles.paymentRow}>
           {/* Яндекс Пэй */}
           <div className={styles.paymentCard}>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/images/payment-yandex-pay.jpg"
-              alt="Яндекс Пэй"
-              className={styles.paymentBg}
-            />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/images/logo-yandex-pay.png"
-              alt="Яндекс Пэй"
-              className={styles.paymentLogo}
-            />
+            {}
+            <picture>
+              <source media="(max-width: 768px)" srcSet="/images/yandex-banner-mob.png" />
+              <img src="/images/yandex-banner.png" alt="Яндекс Пэй" className={styles.paymentBg} />
+            </picture>
           </div>
 
           {/* Яндекс Сплит */}
           <div className={styles.paymentCard}>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/images/payment-yandex-split.jpg"
-              alt="Яндекс Сплит"
-              className={styles.paymentBg}
-            />
+            {}
+            <picture>
+              <source media="(max-width: 768px)" srcSet="/images/split-banner-mob.png" />
+              <img src="/images/split-banner.png" alt="Яндекс Сплит" className={styles.paymentBg} />
+            </picture>
           </div>
         </div>
       </div>
